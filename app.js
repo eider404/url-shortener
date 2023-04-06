1f3501e832d581eb9e035dc00b97b0467ef4f536
Eider Pool <eider.pool.arjona@gmail.com>
2023-04-05 21:39:31 -0600

    coneccion a la db

--- app.js
+++ app.js
@@ -2,6 +2,12 @@ const express = require('express');
 const app = express();
 
 const routes = require('./src/routes');
+const connection = require('./src/connectDB')
+
+
+connection.connect(()=>{
+    console.log(`DB conectada !`);
+});
 
 
 app.use(express.json());
