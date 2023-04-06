43e395d9c9700d8a8052ad98bf5870b6b6fd4fb7
Eider Pool <eider.pool.arjona@gmail.com>
2023-04-06 14:22:50 -0600

    iterar y generar las nuevas URL

--- src/controller/bulkUrlController.js
+++ src/controller/bulkUrlController.js
@@ -1,6 +1,8 @@
 const express = require('express');
 
-const bulkUrl = (req, res) => {
+const generarURL = require("../funtions/generarURL");
+
+const bulkUrl = async(req, res) => {
     
     const archivo = req.file;
     if (!archivo) {
@@ -9,8 +11,21 @@ const bulkUrl = (req, res) => {
     }
     // Obtener el contenido del archivo en memoria
     const buffer = archivo.buffer;
-    const URLS = buffer.toString('utf-8').split('\n'); //array
-    res.json({res: URLS});
+    const URLs = buffer.toString('utf-8').split('\n'); //array
+    
+    var URLsShort = [];
+
+    for (const iterator of URLs) {
+        const isCorrect = await generarURL(iterator);
+        console.log(isCorrect);
+        URLsShort.push(isCorrect);
+
+        //agragar los url valido a la db
+    }
+
+    
+    
+    res.json({input: URLs, output: URLsShort});
     
 }
 
