be321bc9ccb2da0b3c384c4a49f005535d6b304e
Eider Pool <eider.pool.arjona@gmail.com>
2023-04-07 11:58:36 -0600

    controller bulkUrl terminado

--- src/controller/bulkUrlController.js
+++ src/controller/bulkUrlController.js
@@ -1,31 +1,60 @@
 const express = require('express');
 
 const generarURL = require("../funtions/generarURL");
+const connection = require("../connectDB");
+
+
 
 const bulkUrl = async(req, res) => {
     
     const archivo = req.file;
+    var URLs = [];
+    var URLsShort = [];
+
     if (!archivo) {
         res.status(400).send('No se ha subido ningún archivo');
         return;
     }
-    // Obtener el contenido del archivo en memoria
+
     const buffer = archivo.buffer;
-    const URLs = buffer.toString('utf-8').split('\n'); //array
+    URLs = buffer.toString('utf-8').split('\n'); //array
     
-    var URLsShort = [];
 
-    for (const iterator of URLs) {
-        const isCorrect = await generarURL(iterator);
-        console.log(isCorrect);
-        URLsShort.push(isCorrect);
+    for (const url of URLs) {
+        const urlShort = await generarURL(url);
+        let urlForSave = {};
+        
+        if(urlShort == false){
+            URLsShort.push("URL invalida");
+        }else{
 
-        //agragar los url valido a la db
+            await new Promise((resolve, reject) => {
+                connection.query("SELECT * FROM url WHERE urlReal = ?", [url], (err, result, fields) => {
+                    if (err) {
+                        reject(err);
+                    }
+
+                    if (result.length > 0) {
+                        URLsShort.push(result[0].urlShort);
+                    }else{
+                        urlForSave.urlReal = url;
+                        urlForSave.urlShort = urlShort;
+                        URLsShort.push(urlShort);
+                        
+                        connection.query("INSERT INTO url set ?", [urlForSave], (err, rows)=>{
+                            if(err){}
+                            console.log("url insertado");
+                        })
+                    };
+                    resolve();    
+
+                });
+            });
+        } 
     }
 
     
-    
-    res.json({input: URLs, output: URLsShort});
+    return await res.json({input: URLs, output: URLsShort});
     
 }
 
